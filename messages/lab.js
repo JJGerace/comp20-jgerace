c0d4f0241b4989b112bc393bab900d8a3524ef33
JJGerace <jacobjgerace@gmail.com>
2016-03-10 21:03:22 -0500

    attempted to ass XMLhttprequest, dummy parsing function

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+function parse () {
+    var req = new XMLHttpRequest();
+    var loc = "data.json";
+
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            var arrData = JSON.parse(req.responseText);
+            printJSONdata(arrData);
+        }
+    };
+
+    req.open("GET", loc, true);
+    req.send();
+}
+
+function printJSONdata(arrData) {
+    console.log("worked");
+}
